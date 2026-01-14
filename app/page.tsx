d587b7b0b18fd2f03dad5e517003296c3f0dd051
Juseth Porto <jusethporto2@gmail.com>
2026-01-14 13:19:30 -0500

    create a interface simple

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useEffect, useState } from "react";
+
+export default function UsersPage() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/backend/coder")
+      .then(res => res.json())
+      .then(setUsers);
+  }, []);
+
   return (
-    <div></div>
+    <div>
+      <h1>Usuarios</h1>
+      <ul>
+        {users.map((u: any) => (
+          <li key={u._id}>{u.nombre} - {u.correo}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
